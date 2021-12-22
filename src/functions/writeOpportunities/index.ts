ff04e77ba736088bce5c770dc6a97eace45bd70b
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2021-12-22 11:55:37 +0100

    configure access token for hubspot

--- src/functions/writeOpportunities/index.ts
+++ src/functions/writeOpportunities/index.ts
@@ -1,20 +1,21 @@
-import { handlerPath } from '@libs/handlerResolver';
+import { handlerPath } from "@libs/handlerResolver";
+import { stack } from "serverless";
+import { hubspotAccessToken } from "src/resources/ssm";
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
   events: [
     {
       eventBridge: {
-        eventBus: '${construct:hubspot-webhook.busName}',
+        eventBus: "${construct:hubspot-webhook.busName}",
         pattern: {
-          'detail-type': [
-            'Webhook',
-          ],
+          "detail-type": ["Webhook"],
         },
       },
     },
   ],
   environment: {
-    BUCKET_NAME: '${construct:hubspot-s3.bucketName}'
-  }
-}
+    BUCKET_NAME: "ace-apn-1588143-beta-us-west-2",
+    HUBSPOT_ACCESS_TOKEN_PATH: stack.resolve(hubspotAccessToken.parameterName),
+  },
+};
