27448d79a75a88930e7e10ac4c46032fcaf163b9
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2021-12-22 11:53:52 +0100

    create oportunity types

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -1,7 +1,7 @@
-import { EventBridgeEvent } from 'aws-lambda/trigger/eventbridge';
+import { EventBridgeEvent } from "aws-lambda/trigger/eventbridge";
 
 export type WebhookEventBridgeEvent = EventBridgeEvent<
-  'Webhook',
+  "Webhook",
   HubspotWebhook
 >;
 
@@ -9,9 +9,42 @@ export interface HubspotWebhook {
   portalId: number;
   objectType: string;
   objectTypeId: string;
-  objectId: number;
-  properties: Object;
+  objectId: string;
+  properties: {
+    dealname: {
+      value: string;
+    };
+  };
   version: number;
   secondaryIdentifier: null;
-  isDeleted: boolean
+  isDeleted: boolean;
+}
+
+export interface AceFileOppurtunityInbound {
+  version: string;
+  spmsId: string;
+  opportunities: Array<OpportunityInbound>;
+}
+
+export interface OpportunityInbound {
+  status: string;
+  customerCompanyName: string;
+  customerTitle: string | undefined;
+  customerPhone: string | undefined;
+  customerLastName: string | undefined;
+  customerFirstName: string | undefined;
+  customerEmail: string | undefined;
+  customerWebsite: string;
+  partnerProjectTitle: string;
+}
+
+export interface Company {
+  city: string | undefined;
+  createdate: Date | undefined;
+  domain: string | undefined;
+  hs_lastmodifieddate: Date | undefined;
+  industry: string | undefined;
+  name: string | undefined;
+  phone: string | undefined;
+  state: string | undefined;
 }
