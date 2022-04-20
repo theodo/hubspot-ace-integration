ea05401545762a3e4733c9a45554f5e28418af78
Adele Gauvrit <gauvrit.adele@gmail.com>
2022-04-20 14:52:13 +0200

    update ace id in hubspot

--- src/functions/updateHubspotWithApnId/handler.ts
+++ src/functions/updateHubspotWithApnId/handler.ts
@@ -1,11 +1,28 @@
+import { Client } from "@hubspot/api-client";
 import { opportunityCreatedEvent } from "@libs/event";
 import { middyfy } from "@libs/lambda";
 import { PublishedEvent } from "typebridge";
 
+type OpportunityCreatedEvent = PublishedEvent<typeof opportunityCreatedEvent>;
+
 export const updateHubspotWithApnId = async (
-  event: PublishedEvent<typeof opportunityCreatedEvent>
+  event: OpportunityCreatedEvent
 ): Promise<void> => {
   console.log("Event", event);
+  const hubspotClient = new Client({
+    accessToken: process.env.HUBSPOT_ACCESS_TOKEN,
+  });
+
+  const { partnerCrmUniqueIdentifier: dealId, apnCrmUniqueIdentifier: apnId } =
+    event.detail;
+
+  const inputHubspotUpdate = {
+    properties: {
+      identifiant_ace: apnId,
+    },
+  };
+  console.log(`Update hubspot deal ${dealId} with ACE id ${apnId}`);
+  await hubspotClient.crm.deals.basicApi.update(dealId, inputHubspotUpdate);
 
   return;
 };
